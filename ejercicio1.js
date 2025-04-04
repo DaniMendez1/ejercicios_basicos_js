956a8a73f9e52480d3370be54669cd29446ad6e7
DaniMendez1 <danimendez2882@gmail.com>
2025-04-05 00:20:38 +0200

    1

---
+++ ejercicio1.js
@@ -0,0 +1,8 @@
+
+const miSuperHeroeFavorito = "Hulk";
+const numFav = 50;
+const altura = 5;
+const longitud = 10;
+const suma = altura + longitud;
+
+console.log({miSuperHeroeFavorito, numFav, altura, longitud, suma});
